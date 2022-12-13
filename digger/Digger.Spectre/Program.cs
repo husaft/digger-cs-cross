ecfba14d04641e897cf563099bc38d8c83f021ef
Hubertus Sack <hubertussack@outlook.com>
2022-12-13 03:02:22 +0100

    Added new spectre project

---
+++ digger/Digger.Spectre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Digger.Spectre
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
