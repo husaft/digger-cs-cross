819ce8c3af527ad0dc3e9c930ec9cc898bdd78a9
Hubertus Sack <hubertussack@outlook.com>
2022-12-13 11:39:05 +0100

    Added some kind of camera

--- digger/Digger.Spectre/ConDigger.cs
+++ digger/Digger.Spectre/ConDigger.cs
@@ -1,6 +1,8 @@
-﻿using Digger.API;
+﻿using System;
+using Digger.API;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 using Spectre.Console;
 
 // ReSharper disable InconsistentNaming
@@ -28,6 +30,29 @@ namespace Digger.Spectre
             var data = pc.GetPixels().GetRgba32(model);
 
             var image = Image.LoadPixelData<Rgba32>(data, w, h);
+            var play = pc.GetPlayer();
+            const int boxSize = 20;
+            const int moxSizeX = 8 * boxSize;
+            const int moxSizeY = 5 * boxSize;
+            if (play.vis)
+                image.Mutate(c =>
+                {
+                    var ax = play.x - boxSize;
+                    if (ax < 0)
+                        ax = Math.Max(0, ax);
+                    var ay = play.y - boxSize;
+                    if (ay < 0)
+                        ay = Math.Max(0, ay);
+
+                    var max = ax + moxSizeX;
+                    if (max > w)
+                        ax -= max - w;
+                    var may = ay + moxSizeY;
+                    if (may > h)
+                        ay -= may - h;
+
+                    c.Crop(new Rectangle(ax, ay, moxSizeX, moxSizeY));
+                });
             using var old = _bitmap.Exchange(image);
 
             return _bitmap;
